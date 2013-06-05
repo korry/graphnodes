b121f736676509131b6cbe2bedbfe73343e40445
Korry Douglas <korry.douglas@enterprisedb.com>
2013-06-05 13:27:11 -0700

    Untabifying to make online browsing a bit more user-friendly

--- rfmt.h
+++ rfmt.h
@@ -1,28 +1,28 @@
 typedef struct
 {
-	int	  tokenType;
-	char *value;
-	int	  intValue;
+    int   tokenType;
+    char *value;
+    int   intValue;
 } node;
 
 typedef struct
 {
-	int	  tokenType;
-	char *name;
-	node *value;
+    int   tokenType;
+    char *name;
+    node *value;
 } field;
 
 
 typedef struct listCell
 {
-	node			*payload;
-	struct listCell *next;
+    node            *payload;
+    struct listCell *next;
 } listCell;
 
 typedef struct
 {
-	int		  tokenType;
-	int		  length;
-	listCell *head;
-	listCell *tail;
+    int       tokenType;
+    int       length;
+    listCell *head;
+    listCell *tail;
 } list;
